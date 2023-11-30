f52c7bb142e019f9dd6d6f9d73ff3874bf33b529
jbloom-md <joseph@massdynamics.com>
2023-11-30 10:58:30 +0000

    improve metrics

--- sae_training/train_sae.py
+++ sae_training/train_sae.py
@@ -41,24 +41,25 @@ def train_sae(sae: SAE,
             with torch.no_grad():
                 
                 batch_size = batch.shape[0]
-                feature_mean_activation = hidden_post.mean(dim=0)
-                n_dead_features = (feature_mean_activation == 0).sum()
+                frac_feature_activation = (hidden_post > 0).float().mean(0)
+                log_frac_feature_activation = torch.log(frac_feature_activation + 1e-8)
+                n_dead_features = (frac_feature_activation >  0).sum()
                 l0 = ((hidden_post != 0) / batch_size).sum()
                 l2_norm = torch.norm(hidden_post, dim=1).mean()
                 
                 
             if use_wandb and (step % wandb_log_freq == 0):
                 wandb.log({
-                    "mse_loss": mse_loss.item(),
-                    "l1_loss": l1_loss.item(),
-                    "loss": loss.item(),
-                    "l0":  l0.item(),
-                    "l2": l2_norm.item(),
-                    "hist": wandb.Histogram(feature_mean_activation.tolist()),
-                    "n_dead_features": n_dead_features,
+                    "losses/mse_loss": mse_loss.item(),
+                    "losses/l1_loss": l1_loss.item(),
+                    "losses/overall_loss": loss.item(),
+                    "metrics/l0":  l0.item(),
+                    "metrics/l2": l2_norm.item(),
+                    "metrics/feature_density_histogram": wandb.Histogram(log_frac_feature_activation.tolist()),
+                    "metrics/n_dead_features": n_dead_features,
                 }, step=n_training_steps)
                 
-            pbar.set_description(f"Epoch {epoch} | Step {step} | MSE Loss {mse_loss.item():.3f} | L1 Loss {l1_loss.item():.3f} | L0 {l0.item():.3f} | n_dead_features {n_dead_features}")
+            pbar.set_description(f"{epoch}/{step}| MSE Loss {mse_loss.item():.3f} | L0 {l0.item():.3f} | n_dead_features {n_dead_features}")
             
             loss.backward()
             
