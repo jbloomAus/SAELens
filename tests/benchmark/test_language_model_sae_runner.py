f6013625963ebabdb86063f8db7a5e8339e37873
Joseph Bloom <jbloomaus@gmail.com>
2023-12-01 22:00:29 +0000

    test pars

--- tests/benchmark/test_language_model_sae_runner.py
+++ tests/benchmark/test_language_model_sae_runner.py
@@ -6,24 +6,73 @@ from sae_training.lm_runner import (
 )
 
 
-def test_language_model_sae_runner():
+def test_language_model_sae_runner_mlp_out():
     
     
     cfg = LanguageModelSAERunnerConfig(
 
         # Data Generating Function (Model + Training Distibuion)
         model_name = "gelu-2l",
-        hook_point = "blocks.0.hook_mlp_out",
+        hook_point = "blocks.1.hook_mlp_out",
         hook_point_layer = 0,
         d_in = 512,
         dataset_path = "NeelNanda/c4-tokenized-2b",
         
         # SAE Parameters
-        expansion_factor = 16, # determines the dimension of the SAE.
+        expansion_factor = 64, # determines the dimension of the SAE.
         
         # Training Parameters
         lr = 1e-4,
-        l1_coefficient = 3e-4,
+        l1_coefficient = 1e-4,
+        train_batch_size = 4096,
+        context_size = 128,
+        
+        # Activation Store Parameters
+        n_batches_in_buffer = 8,
+        total_training_tokens = 25_000_00 * 60,
+        store_batch_size = 32,
+        
+        # Resampling protocol
+        feature_sampling_window = 1000,
+        feature_reinit_scale = 0.2,
+        dead_feature_threshold = 1e-8,
+        
+        # WANDB
+        log_to_wandb = True,
+        wandb_project= "mats_sae_training_language_models",
+        wandb_entity = None,
+        
+        # Misc
+        device = "cuda",
+        seed = 42,
+        checkpoint_path = "checkpoints",
+        dtype = torch.float32,
+        )
+
+    trained_sae = language_model_sae_runner(cfg)
+
+    assert trained_sae is not None
+
+
+
+def test_language_model_sae_runner_resid_pre():
+    
+    
+    cfg = LanguageModelSAERunnerConfig(
+
+        # Data Generating Function (Model + Training Distibuion)
+        model_name = "gelu-2l",
+        hook_point = "blocks.0.hook_resid_pre",
+        hook_point_layer = 0,
+        d_in = 512,
+        dataset_path = "NeelNanda/c4-tokenized-2b",
+        
+        # SAE Parameters
+        expansion_factor = 32, # determines the dimension of the SAE.
+        
+        # Training Parameters
+        lr = 1e-4,
+        l1_coefficient = 1e-4,
         train_batch_size = 4096,
         context_size = 128,
         
@@ -47,6 +96,5 @@ def test_language_model_sae_runner():
     trained_sae = language_model_sae_runner(cfg)
 
     assert trained_sae is not None
-
 
 
