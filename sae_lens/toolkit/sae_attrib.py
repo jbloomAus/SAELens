6b4150caf283e96375234609dc0436399e008118
Daniel CH Tan <dtch1997@users.noreply.github.com>
2024-04-15 14:56:15 +0100

    refactor: compute node ie

--- sae_lens/toolkit/sae_attrib.py
+++ sae_lens/toolkit/sae_attrib.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from typing import Callable, Protocol
 
 import torch
@@ -5,7 +6,7 @@ from jaxtyping import Float
 from transformer_lens import HookedTransformer
 from transformer_lens.hook_points import HookPoint
 
-from sae_lens import SparseAutoencoder
+from sae_lens import SparseAutoencoder, SparseAutoencoderDictionary
 
 
 class TransformerLensForwardHook(Protocol):
@@ -96,45 +97,116 @@ class SAEPatcher:
         """Return a backward hook that patches the gradients."""
         return (self.sae.cfg.hook_point, self._backward_hook_fn)
 
-    def get_ie_atp_of_sae_features(
-        self, z_patch: Float[torch.Tensor, "n_batch n_token d_sae"]
-    ) -> Float[torch.Tensor, "n_batch n_token d_sae"]:
-        grad = self.sae_feature_acts.grad
-        val = self.sae_feature_acts
-        return grad * (z_patch - val)
 
-    def get_ie_atp_of_sae_errors(
-        self, a_patch: Float[torch.Tensor, "n_batch n_token d_model"]
-    ) -> Float[torch.Tensor, "n_batch n_token d_model"]:
-        grad = self.sae_errors.grad
-        val = self.sae_errors.grad
-        return grad * (a_patch - val)
-
-    # TODO: implement integrated gradients
+def indirect_effect_attrib(
+    t_orig: Float[torch.Tensor, "n_batch n_token d"],
+    t_patch: Float[torch.Tensor, "n__batch n_token d"],
+    grad_m_t_at_t_orig: Float[torch.Tensor, "n_batch n_token d"],
+):
+    return grad_m_t_at_t_orig * (t_patch - t_orig)
 
 
-def compute_indirect_effect(
+@dataclass
+class SAELayerAttributionScores:
+    name: str
+    feature_scores: Float[torch.Tensor, "n_batch n_token d_sae"]
+    error_scores: Float[torch.Tensor, "n_batch n_token d_model"]
+
+
+def get_sae_features_and_errors_for_input(
     model: HookedTransformer,
-    sae: SparseAutoencoder,
+    sae_dict: SparseAutoencoderDictionary,
     metric_fn: Callable[[HookedTransformer, str], Float[torch.Tensor, ""]],
-    x_orig: str,
-    z_patch: Float[torch.Tensor, "n_batch n_token d_sae"] | None = None,
-    err_patch: Float[torch.Tensor, "n_batch n_token d_model"] | None = None,
+    x: str,
 ) -> tuple[
-    Float[torch.Tensor, "n_batch n_token d_sae"],
-    Float[torch.Tensor, "n_batch n_token d_model"],
+    dict[str, Float[torch.Tensor, "n_batch n_token d_sae"]],
+    dict[str, Float[torch.Tensor, "n_batch n_token d_model"]],
 ]:
-    sae_patcher = SAEPatcher(sae)
-    with model.hooks(fwd_hooks=[sae_patcher.get_forward_hook()]):
-        metric = metric_fn(model, x_orig)
+    # Run model on original input
+    patchers = {name: SAEPatcher(sae) for name, sae in sae_dict}
+    with model.hooks(
+        fwd_hooks=[p.get_forward_hook() for p in patchers.values()],
+        bwd_hooks=[p.get_backward_hook() for p in patchers.values()],
+    ):
+        metric = metric_fn(model, x)
         metric.backward()
 
-    if z_patch is None:
-        z_patch = torch.zeros_like(sae_patcher.sae_feature_acts)
-    if err_patch is None:
-        err_patch = torch.zeros_like(sae_patcher.sae_errors)
+    # Collate the sae features and errors
+    orig_features = {
+        name: patcher.sae_feature_acts for name, patcher in patchers.items()
+    }
+    orig_errors = {name: patcher.sae_errors for name, patcher in patchers.items()}
+    return orig_features, orig_errors
 
-    ie_atp_feat = sae_patcher.get_ie_atp_of_sae_features(z_patch)
-    ie_atp_err = sae_patcher.get_ie_atp_of_sae_errors(err_patch)
-    return ie_atp_feat, ie_atp_err
-    return ie_atp_feat, ie_atp_err
+
+def compute_node_indirect_effect(
+    model: HookedTransformer,
+    sae_dict: SparseAutoencoderDictionary,
+    metric_fn: Callable[[HookedTransformer, str], Float[torch.Tensor, ""]],
+    x_orig: str,
+    x_patch: str | None = None,
+) -> tuple[
+    dict[str, Float[torch.Tensor, "n_batch n_token d_sae"]],
+    dict[str, Float[torch.Tensor, "n_batch n_token d_model"]],
+]:
+    """Compute node indirect effects for a given input
+
+    Here, nodes are the features and errors of the SAEs
+    and the indirect effect is computed using first-order Taylor approximation.
+
+    Returns:
+    - feature_scores: The scores for each feature.
+      A dict of {name: [batch, token, d_sae]} of feature scores
+    - error_scores: The scores for each error
+    """
+    orig_features, orig_errors = get_sae_features_and_errors_for_input(
+        model, sae_dict, metric_fn, x_orig
+    )
+
+    # If no patch is provided, return zeros
+    if x_patch is None:
+        patch_features = {
+            name: torch.zeros_like(act) for name, act in orig_features.items()
+        }
+        patch_errors = {
+            name: torch.zeros_like(err) for name, err in orig_errors.items()
+        }
+    else:
+        patch_features, patch_errors = get_sae_features_and_errors_for_input(
+            model, sae_dict, metric_fn, x_patch
+        )
+
+    # Compute feature scores
+    feature_scores = {}
+    for name, orig_act in orig_features.items():
+        grad_m_orig_act = orig_act.grad
+        assert grad_m_orig_act is not None
+        patch_act = patch_features[name]
+        feature_scores[name] = indirect_effect_attrib(
+            orig_act, patch_act, grad_m_orig_act
+        )
+
+    # Compute error scores
+    error_scores = {}
+    for name, orig_err in orig_errors.items():
+        grad_m_orig_err = orig_err.grad
+        assert grad_m_orig_err is not None
+        patch_err = patch_errors[name]
+        error_scores[name] = indirect_effect_attrib(
+            orig_err, patch_err, grad_m_orig_err
+        )
+
+    return feature_scores, error_scores
+
+
+# def compute_edge_indirect_effect(
+#     model: HookedTransformer,
+#     sae_dict: SparseAutoencoderDictionary,
+#     metric_fn: Callable[[HookedTransformer, str], Float[torch.Tensor, ""]],
+#     x_orig: str,
+#     x_patch: str | None = None,
+# ) -> tuple[
+#     dict[str, Float[torch.Tensor, "n_batch n_token d_sae"]],
+#     dict[str, Float[torch.Tensor, "n_batch n_token d_model"]],
+# ]:
+#     pass
