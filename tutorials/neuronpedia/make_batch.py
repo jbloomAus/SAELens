b611e721dd2620ab5a030cc0f6e37029c30711ca
Johnny Lin <hijohnnylin@gmail.com>
2024-04-17 00:30:46 -0700

    Use python typer instead of shell script for neuronpedia jobs

--- tutorials/neuronpedia/make_batch.py
+++ tutorials/neuronpedia/make_batch.py
@@ -1,9 +1,12 @@
 import sys
 from sae_lens.analysis.neuronpedia_runner import NeuronpediaRunner
 
-SAE_PATH = sys.argv[1]
-MODEL_ID = sys.argv[2]
-SAE_ID = sys.argv[3]
+# we use another python script to launch this using subprocess to work around OOM issues - this ensures every batch gets the whole system available memory
+# better fix is to investigate and fix the memory issues
+
+SAE_ID = sys.argv[1]
+SAE_PATH = sys.argv[2]
+OUTPUTS_DIR = sys.argv[3]
 SPARSITY_THRESHOLD = int(sys.argv[4])
 N_BATCHES_SAMPLE = int(sys.argv[5])
 N_PROMPTS_SELECT = int(sys.argv[6])
@@ -11,15 +14,11 @@ FEATURES_AT_A_TIME = int(sys.argv[7])
 START_BATCH_INCLUSIVE = int(sys.argv[8])
 END_BATCH_INCLUSIVE = int(sys.argv[9])
 
-NP_OUTPUT_FOLDER = "../../neuronpedia_outputs"
-
 runner = NeuronpediaRunner(
-    sae_path=SAE_PATH,
-    model_id=MODEL_ID,
     sae_id=SAE_ID,
+    sae_path=SAE_PATH,
+    outputs_dir=OUTPUTS_DIR,
     sparsity_threshold=SPARSITY_THRESHOLD,
-    neuronpedia_outputs_folder=NP_OUTPUT_FOLDER,
-    init_session=True,
     n_batches_to_sample_from=N_BATCHES_SAMPLE,
     n_prompts_to_select=N_PROMPTS_SELECT,
     n_features_at_a_time=FEATURES_AT_A_TIME,
