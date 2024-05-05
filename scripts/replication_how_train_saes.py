7c9f32a3738c71eb531b905223b45de15b3feb81
Joseph Bloom <jbloomaus@gmail.com>
2024-05-05 17:58:53 +0000

    current script

--- scripts/replication_how_train_saes.py
+++ scripts/replication_how_train_saes.py
@@ -18,7 +18,7 @@ else:
 print("Using device:", device)
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
-total_training_steps = 20_000
+total_training_steps = 200_000
 batch_size = 4096
 total_training_tokens = total_training_steps * batch_size
 print(f"Total Training Tokens: {total_training_tokens}")
@@ -35,76 +35,83 @@ lr_decay_steps = total_training_steps // 5  # 20% of training steps.
 print(f"lr_decay_steps: {lr_decay_steps}")
 l1_warmup_steps = total_training_steps // 20  # 5% of training steps.
 print(f"l1_warmup_steps: {l1_warmup_steps}")
-
 log_to_wandb = True
 
-cfg = LanguageModelSAERunnerConfig(
-    # Pick a tiny model to make this easier.
-    model_name=model_name,
-    ## MLP Layer 0 ##
-    hook_point="blocks.0.hook_mlp_out",
-    hook_point_layer=0,
-    d_in=512,
-    dataset_path=dataset_path,
-    context_size=1024,
-    is_dataset_tokenized=True,
-    prepend_bos=False,  # I used to train GPT2 SAEs with a prepended-bos but no longer think we should do this.
-    # How big do we want our SAE to be?
-    expansion_factor=16,
-    # Dataset / Activation Store
-    # For now.
-    use_cached_activations=True,
-    cached_activations_path=new_cached_activations_path,
-    training_tokens=total_training_tokens,  # For initial testing I think this is a good number.
-    train_batch_size=4096,
-    # Loss Function
-    ## Reconstruction Coefficient.
-    mse_loss_normalization=None,  # MSE Loss Normalization is not mentioned (so we use stanrd MSE Loss). But not we take an average over the batch.
-    ## Anthropic does not mention using an Lp norm other than L1.
-    l1_coefficient=5,
-    lp_norm=1.0,
-    # Instead, they multiply the L1 loss contribution
-    # from each feature of the activations by the decoder norm of the corresponding feature.
-    scale_sparsity_penalty_by_decoder_norm=True,
-    # Learning Rate
-    lr_scheduler_name="constant",  # we set this independently of warmup and decay steps.
-    l1_warm_up_steps=l1_warmup_steps,
-    lr_warm_up_steps=lr_warm_up_steps,
-    lr_decay_steps=lr_warm_up_steps,
-    ## No ghost grad term.
-    use_ghost_grads=False,
-    # Initialization / Architecture
-    apply_b_dec_to_input=False,
-    # encoder bias zero's. (I'm not sure what it is by default now)
-    # decoder bias zero's.
-    b_dec_init_method="zeros",
-    normalize_sae_decoder=False,
-    decoder_heuristic_init=True,
-    init_encoder_as_decoder_transpose=True,
-    # Optimizer
-    lr=5e-5,
-    ## adam optimizer has no weight decay by default so worry about this.
-    adam_beta1=0.9,
-    adam_beta2=0.999,
-    # Buffer details won't matter in we cache / shuffle our activations ahead of time.
-    n_batches_in_buffer=64,
-    store_batch_size=16,
-    normalize_activations=False,
-    # Feature Store
-    feature_sampling_window=1000,
-    dead_feature_window=1000,
-    dead_feature_threshold=1e-4,
-    # WANDB
-    log_to_wandb=log_to_wandb,  # always use wandb unless you are just testing code.
-    wandb_project="how_we_train_SAEs_replication_1",
-    wandb_log_frequency=50,
-    # Misc
-    device=device,
-    seed=42,
-    n_checkpoints=0,
-    checkpoint_path="checkpoints",
-    dtype=torch.float32,
-)
+for l1_coefficient in [2, 5, 10]:
 
-# look at the next cell to see some instruction for what to do while this is running.
-sparse_autoencoder_dictionary = language_model_sae_runner(cfg)
+    cfg = LanguageModelSAERunnerConfig(
+        # Pick a tiny model to make this easier.
+        model_name="gelu-1l",
+        ## MLP Layer 0 ##
+        hook_point="blocks.0.hook_mlp_out",
+        hook_point_layer=0,
+        d_in=512,
+        dataset_path="NeelNanda/c4-tokenized-2b",
+        streaming=False,
+        context_size=1024,
+        is_dataset_tokenized=True,
+        prepend_bos=True,
+        # How big do we want our SAE to be?
+        expansion_factor=64,
+        # Dataset / Activation Store
+        # When we do a proper test
+        # training_tokens= 820_000_000, # 200k steps * 4096 batch size ~ 820M tokens (doable overnight on an A100)
+        # For now.
+        use_cached_activations=True,
+        cached_activations_path="/home/paperspace/shared_volumes/activations_volume_1/gelu-1l",
+        training_tokens=total_training_tokens,  # For initial testing I think this is a good number.
+        train_batch_size=4096,
+        # Loss Function
+        ## Reconstruction Coefficient.
+        mse_loss_normalization=None,  # MSE Loss Normalization is not mentioned (so we use stanrd MSE Loss). But not we take an average over the batch.
+        ## Anthropic does not mention using an Lp norm other than L1.
+        l1_coefficient=l1_coefficient,
+        lp_norm=1.0,
+        # Instead, they multiply the L1 loss contribution
+        # from each feature of the activations by the decoder norm of the corresponding feature.
+        scale_sparsity_penalty_by_decoder_norm=True,
+        # Learning Rate
+        lr_scheduler_name="constant",  # we set this independently of warmup and decay steps.
+        l1_warm_up_steps=l1_warmup_steps,
+        lr_warm_up_steps=lr_warm_up_steps,
+        lr_decay_steps=lr_warm_up_steps,
+        ## No ghost grad term.
+        use_ghost_grads=False,
+        # Initialization / Architecture
+        apply_b_dec_to_input=False,
+        # encoder bias zero's. (I'm not sure what it is by default now)
+        # decoder bias zero's.
+        b_dec_init_method="zeros",
+        normalize_sae_decoder=False,
+        decoder_heuristic_init=True,
+        init_encoder_as_decoder_transpose=True,
+        # Optimizer
+        lr=5e-5,
+        ## adam optimizer has no weight decay by default so worry about this.
+        adam_beta1=0.9,
+        adam_beta2=0.999,
+        # Buffer details won't matter in we cache / shuffle our activations ahead of time.
+        n_batches_in_buffer=64,
+        store_batch_size=16,
+        normalize_activations=False,
+        # Feature Store
+        feature_sampling_window=1000,
+        dead_feature_window=1000,
+        dead_feature_threshold=1e-4,
+        # WANDB
+        log_to_wandb=log_to_wandb,  # always use wandb unless you are just testing code.
+        wandb_project="how_we_train_SAEs_replication_1",
+        wandb_log_frequency=50,
+        eval_every_n_wandb_logs=10,
+        # Misc
+        device=device,
+        seed=42,
+        n_checkpoints=0,
+        checkpoint_path="checkpoints",
+        dtype=torch.float32,
+    )
+
+    # look at the next cell to see some instruction for what to do while this is running.
+    sparse_autoencoder_dictionary = language_model_sae_runner(cfg)
+
+    print("="*50)
