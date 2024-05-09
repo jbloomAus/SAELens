a95664dfbc3a85b21e0ca7b198e008cca74ba587
tomMcGrath <thomas.m.mcgrath@gmail.com>
2024-05-09 21:18:54 +0100

    missed one!

--- tests/unit/training/test_activations_store.py
+++ tests/unit/training/test_activations_store.py
@@ -97,7 +97,7 @@ def test_activations_store__shapes_look_correct_with_real_models_and_datasets(
     #
     # cfg.context_size = 1024
     # cfg.n_batches_in_buffer = 64
-    # cfg.store_batch_size = 16
+    # cfg.store_batch_size_prompts = 16
 
     store = ActivationsStore.from_config(model, cfg)
 
@@ -190,7 +190,7 @@ def test_activations_store__get_batch_tokens__fills_the_context_separated_by_bos
         * 100
     )
     cfg = build_sae_cfg(
-        store_batch_size=2,
+        store_batch_size_prompts=2,
         context_size=context_size,
     )
 
