10bd9c58fd4d731bd453e49943d40f2ac01ff0fc
Joseph Bloom <69127271+jbloomAus@users.noreply.github.com>
2024-06-13 13:18:30 +0100

    add expected perf for pretrained (#179)
    Co-authored-by: jbloom-md <joseph@massdynamics.com>

--- sae_lens/toolkit/pretrained_saes_directory.py
+++ sae_lens/toolkit/pretrained_saes_directory.py
@@ -12,6 +12,8 @@ class PretrainedSAELookup:
     model: str
     conversion_func: str | None
     saes_map: dict[str, str]  # id -> path
+    expected_var_explained: dict[str, float]
+    expected_l0: dict[str, float]
 
 
 @cache
@@ -24,13 +26,21 @@ def get_pretrained_saes_directory() -> dict[str, PretrainedSAELookup]:
         data = yaml.safe_load(file)
         for release, value in data["SAE_LOOKUP"].items():
             saes_map: dict[str, str] = {}
+            var_explained_map: dict[str, float] = {}
+            l0_map: dict[str, float] = {}
             for hook_info in value["saes"]:
                 saes_map[hook_info["id"]] = hook_info["path"]
+                var_explained_map[hook_info["id"]] = hook_info.get(
+                    "variance_explained", 1.00
+                )
+                l0_map[hook_info["id"]] = hook_info.get("l0", 0.00)
             directory[release] = PretrainedSAELookup(
                 release=release,
                 repo_id=value["repo_id"],
                 model=value["model"],
                 conversion_func=value.get("conversion_func"),
                 saes_map=saes_map,
+                expected_var_explained=var_explained_map,
+                expected_l0=l0_map,
             )
     return directory
