c486f05c94c87ebca8cbb5401158872aafb0a4b9
Louka Ewington-Pitsos <lewingtonpitsos@gmail.com>
2024-07-07 08:25:24 +1000

    deduplicate small mathematical operations

--- sae_lens/cache_activations_runner.py
+++ sae_lens/cache_activations_runner.py
@@ -41,23 +41,30 @@ class CacheActivationsRunner:
             if isinstance(self.cfg.dtype, torch.dtype)
             else DTYPE_MAP[self.cfg.dtype].itemsize
         )
-        tokens_in_buffer = (
-            self.cfg.n_batches_in_buffer
-            * self.cfg.store_batch_size_prompts
-            * self.cfg.context_size
-        )
         total_training_tokens = self.cfg.training_tokens
         total_disk_space_gb = total_training_tokens * bytes_per_token / 10**9
 
         return (
             f"Activation Cache Runner:\n"
             f"Total training tokens: {total_training_tokens}\n"
-            f"Number of buffers: {math.ceil(total_training_tokens / tokens_in_buffer)}\n"
-            f"Tokens per buffer: {tokens_in_buffer}\n"
+            f"Number of buffers: {self.n_buffers}\n"
+            f"Tokens per buffer: {self.tokens_in_buffer}\n"
             f"Disk space required: {total_disk_space_gb:.2f} GB\n"
             f"Configuration:\n"
             f"{self.cfg}"
         )
+    
+    @property
+    def tokens_in_buffer(self):
+        return (
+            self.cfg.n_batches_in_buffer
+            * self.cfg.store_batch_size_prompts
+            * self.cfg.context_size
+        )
+
+    @property
+    def n_buffers(self):
+        return math.ceil(self.cfg.training_tokens / self.tokens_in_buffer)
 
     @torch.no_grad()
     def run(self):
@@ -75,22 +82,16 @@ class CacheActivationsRunner:
             os.makedirs(new_cached_activations_path)
 
         print(f"Started caching {self.cfg.training_tokens} activations")
-        tokens_per_buffer = (
-            self.cfg.store_batch_size_prompts
-            * self.cfg.context_size
-            * self.cfg.n_batches_in_buffer
-        )
 
-        n_buffers = math.ceil(self.cfg.training_tokens / tokens_per_buffer)
 
-        for i in tqdm(range(n_buffers), desc="Caching activations"):
+        for i in tqdm(range(self.n_buffers), desc="Caching activations"):
             buffer = self.activations_store.get_buffer()
             buffer_path = f"{new_cached_activations_path}/{i}.{self.file_extension}"
             self.activations_store.save_buffer(buffer, buffer_path)
 
             del buffer
 
-            if i % self.cfg.shuffle_every_n_buffers == 0 and i > 0:
+            if i > 0 and i % self.cfg.shuffle_every_n_buffers == 0:
                 # Shuffle the buffers on disk
 
                 # Do random pairwise shuffling between the last shuffle_every_n_buffers buffers
@@ -108,11 +109,11 @@ class CacheActivationsRunner:
                     )
 
         # More final shuffling (mostly in case we didn't end on an i divisible by shuffle_every_n_buffers)
-        if n_buffers > 1:
+        if self.n_buffers > 1:
             for _ in tqdm(range(self.cfg.n_shuffles_final), desc="Final shuffling"):
                 self.shuffle_activations_pairwise(
                     new_cached_activations_path,
-                    buffer_idx_range=(0, n_buffers),
+                    buffer_idx_range=(0, self.n_buffers),
                 )
 
     @torch.no_grad()
