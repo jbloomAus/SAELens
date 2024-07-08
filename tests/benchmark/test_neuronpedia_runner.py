3126de01d53b250c038d8d7135e33769322fdcfa
curt-tigges <ct@curttigges.com>
2024-07-08 11:51:09 -0700

    Removed reliance on sae-vis

--- tests/benchmark/test_neuronpedia_runner.py
+++
@@ -1,8 +0,0 @@
-# import pytest
-
-# # TODO: get the neuronpedia runner working for any SAE we can load in via SparseAutoencoder.from_pretrained.
-
-
-# @pytest.skip("Not implemented")
-# def test_neuronpedia_runner():
-#     pass
