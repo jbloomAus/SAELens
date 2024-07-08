4ad93452d112f64003d4988c107293ffa1ab8bab
curt-tigges <ct@curttigges.com>
2024-07-08 14:45:42 -0700

    Removed test file

--- test.py
+++
@@ -1,9 +0,0 @@
-# %%
-from sae_lens.sae import SAE
-
-sae = SAE.load_from_pretrained(
-    path="/media/curttigges/project-files/projects/Mistral-7B-Residual-Stream-SAEs/mistral_7b_layer_8",
-    device="cuda:0",
-)
-print(sae.device)
-# %%
