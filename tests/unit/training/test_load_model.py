dc627db537085a7625a52570bc51d6250da344e7
David Chanin <chanindav@gmail.com>
2024-07-11 00:30:29 +0100

    adding load_model helper for huggingface causal LM models

--- tests/unit/training/test_load_model.py
+++ tests/unit/training/test_load_model.py
@@ -1,7 +1,9 @@
 from mamba_lens import HookedMamba
+import torch
 from transformer_lens import HookedTransformer
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-from sae_lens.load_model import load_model
+from sae_lens.load_model import HookedProxyLM, load_model
 
 
 def test_load_model_works_with_mamba():
@@ -35,3 +37,29 @@ def test_load_model_works_with_model_kwargs():
     assert model is not None
     assert isinstance(model, HookedTransformer)
     assert model.cfg.checkpoint_index == 0
+
+
+def test_load_model_with_generic_huggingface_lm():
+    model = load_model(
+        model_class_name="AutoModelForCausalLM",
+        model_name="gpt2",
+        device="cpu",
+    )
+    assert model is not None
+    assert isinstance(model, HookedProxyLM)
+
+
+def test_HookedProxyLM_gives_same_output_as_original_implementation():
+    hf_model = AutoModelForCausalLM.from_pretrained("gpt2")
+    tokenizer = AutoTokenizer.from_pretrained("gpt2")
+    hooked_model = HookedProxyLM(hf_model, tokenizer)
+    input_ids = tokenizer.encode("hi", return_tensors="pt")
+    output, cache = hooked_model.run_with_cache(input_ids)
+
+    hf_output = hf_model(input_ids, output_hidden_states=True)
+
+    assert torch.allclose(output.logits, hf_output.logits)
+    for i in range(len(hf_output.hidden_states) - 2):
+        assert torch.allclose(
+            cache[f"transformer.h.{i}"], hf_output.hidden_states[i + 1]
+        )
