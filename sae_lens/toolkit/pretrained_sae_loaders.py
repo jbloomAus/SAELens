53dbde6ee1520494e12009bb1a39fe603d1a55de
Curt Tigges <curttigges@Curts-MacBook-Pro.local>
2024-10-02 11:01:22 -0700

    Added dataset_trust_remote_code arg

--- sae_lens/toolkit/pretrained_sae_loaders.py
+++ sae_lens/toolkit/pretrained_sae_loaders.py
@@ -446,7 +446,7 @@ def get_dictionary_learning_config_1(
         "context_size": buffer["ctx_len"],
         "normalize_activations": "none",
         "neuronpedia_id": None,
-        "trust_remote_code": True,
+        "dataset_trust_remote_code": True,
     }
 
 
