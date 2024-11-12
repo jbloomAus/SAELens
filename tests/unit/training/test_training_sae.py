17506ac3d0a9401a766bf9ca7ad40f4ef26c85f4
David Chanin <chanindav@gmail.com>
2024-11-12 06:29:52 -0600

    fix: fixing jumprelu encode and save/load (#373)
    * fix: jumprelu encode and save/load
    
    * fixing tests
    
    * changes from CR

--- tests/unit/training/test_training_sae.py
+++ tests/unit/training/test_training_sae.py
@@ -1,6 +1,9 @@
+from pathlib import Path
+
 import pytest
 import torch
 
+from sae_lens.sae import SAE
 from sae_lens.training.training_sae import TrainingSAE, TrainingSAEConfig
 from tests.unit.helpers import build_sae_cfg
 
@@ -37,3 +40,50 @@ def test_TrainingSAE_training_forward_pass_can_scale_sparsity_penalty_by_decoder
             pytest.approx(train_step_output.losses["l1_loss"].detach().item())  # type: ignore
             == 2.0 * feature_acts.norm(p=1, dim=1).mean().detach().item()
         )
+
+
+@pytest.mark.parametrize("architecture", ["standard", "gated", "jumprelu"])
+def test_TrainingSAE_encode_returns_same_value_as_encode_with_hidden_pre(
+    architecture: str,
+):
+    cfg = build_sae_cfg(architecture=architecture)
+    sae = TrainingSAE(TrainingSAEConfig.from_sae_runner_config(cfg))
+    x = torch.randn(32, cfg.d_in)
+    encode_out = sae.encode(x)
+    encode_with_hidden_pre_out = sae.encode_with_hidden_pre_fn(x)[0]
+    assert torch.allclose(encode_out, encode_with_hidden_pre_out)
+
+
+def test_TrainingSAE_initializes_only_with_log_threshold_if_jumprelu():
+    cfg = build_sae_cfg(architecture="jumprelu", jumprelu_init_threshold=0.01)
+    sae = TrainingSAE(TrainingSAEConfig.from_sae_runner_config(cfg))
+    param_names = dict(sae.named_parameters()).keys()
+    assert "log_threshold" in param_names
+    assert "threshold" not in param_names
+    assert torch.allclose(
+        sae.threshold,
+        torch.ones_like(sae.log_threshold.data) * cfg.jumprelu_init_threshold,
+    )
+
+
+def test_TrainingSAE_jumprelu_save_and_load(tmp_path: Path):
+    cfg = build_sae_cfg(architecture="jumprelu")
+    training_sae = TrainingSAE.from_dict(cfg.get_training_sae_cfg_dict())
+
+    training_sae.save_model(str(tmp_path))
+
+    loaded_training_sae = TrainingSAE.load_from_pretrained(str(tmp_path))
+    loaded_sae = SAE.load_from_pretrained(str(tmp_path))
+
+    assert training_sae.cfg.to_dict() == loaded_training_sae.cfg.to_dict()
+    for param_name, param in training_sae.named_parameters():
+        assert torch.allclose(param, loaded_training_sae.state_dict()[param_name])
+
+    test_input = torch.randn(32, cfg.d_in)
+    training_sae_out = training_sae.encode_with_hidden_pre_fn(test_input)[0]
+    loaded_training_sae_out = loaded_training_sae.encode_with_hidden_pre_fn(test_input)[
+        0
+    ]
+    loaded_sae_out = loaded_sae.encode(test_input)
+    assert torch.allclose(training_sae_out, loaded_training_sae_out)
+    assert torch.allclose(training_sae_out, loaded_sae_out)
