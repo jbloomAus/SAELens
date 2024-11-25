20f67530a52416706a79d15dbad55840702312a6
anthonyduong <anthonyduong9@gmail.com>
2024-11-25 01:41:36 -0800

    fixes error for CI

--- sae_lens/toolkit/pretrained_saes.py
+++ sae_lens/toolkit/pretrained_saes.py
@@ -7,7 +7,8 @@ from huggingface_hub import hf_hub_download, list_repo_tree
 from safetensors import safe_open
 from tqdm import tqdm
 
-from sae_lens.sae import SAE, logger
+from sae_lens import logger
+from sae_lens.sae import SAE
 
 
 def load_sparsity(path: str) -> torch.Tensor:
