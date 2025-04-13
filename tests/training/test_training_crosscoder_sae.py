92ae2bde0091f1e277583dfbe3811f4dce69c5a0
Michael Behr <mkbehr@gmail.com>
2025-04-13 15:16:44 -0400

    test_TrainingCrosscoderSAE_encode_returns_same_value_as_encode_with_hidden_pre

--- tests/training/test_training_crosscoder_sae.py
+++ tests/training/test_training_crosscoder_sae.py
@@ -8,13 +8,18 @@ from sae_lens.training.training_crosscoder_sae import (
 )
 from tests.helpers import build_sae_cfg
 
+def build_crosscoder_sae_cfg(**kwargs):
+    return build_sae_cfg(
+        **(kwargs | {
+            "hook_layers": [1,2,3,4],
+            "normalize_sae_decoder": False,
+            "scale_sparsity_penalty_by_decoder_norm": True,
+            }))
+
 def test_TrainingCrosscoderSAE_training_forward_pass_can_scale_sparsity_penalty_by_decoder_norm():
-    cfg = build_sae_cfg(
+    cfg = build_crosscoder_sae_cfg(
         d_in=3,
         d_sae=5,
-        hook_layers=[1,2,3,4],
-        normalize_sae_decoder=False,
-        scale_sparsity_penalty_by_decoder_norm=True,
     )
     training_sae = TrainingCrosscoderSAE(
         TrainingCrosscoderSAEConfig.from_sae_runner_config(cfg),
@@ -36,3 +41,19 @@ def test_TrainingCrosscoderSAE_training_forward_pass_can_scale_sparsity_penalty_
         pytest.approx(train_step_output.losses["l1_loss"].detach().item())  # type: ignore
         == 2.0 * scaled_feature_acts.norm(p=1, dim=1).mean().detach().item()
     )
+
+@pytest.mark.parametrize("architecture", ["standard", "gated", "jumprelu", "topk"])
+def test_TrainingCrosscoderSAE_encode_returns_same_value_as_encode_with_hidden_pre(
+    architecture: str,
+):
+    if architecture != "standard":
+        pytest.xfail("TODO(mkbehr): support other architectures")
+    cfg = build_crosscoder_sae_cfg(architecture=architecture)
+    sae = TrainingCrosscoderSAE(
+        TrainingCrosscoderSAEConfig.from_sae_runner_config(cfg),
+        use_error_term=True,
+    )
+    x = torch.randn(32, len(cfg.hook_layers), cfg.d_in)
+    encode_out = sae.encode(x)
+    encode_with_hidden_pre_out = sae.encode_with_hidden_pre_fn(x)[0]
+    assert torch.allclose(encode_out, encode_with_hidden_pre_out)
