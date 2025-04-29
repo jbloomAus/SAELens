fix: Use the whole batch when estimating norm scaling factor. (#467)
The code was taking the first sample from the batch instead of the
first layer.

sae_lens/training/activations_store.py
@@ -448,7 +448,7 @@ class ActivationsStore:
         ):
             # temporalily set estimated_norm_scaling_factor to 1.0 so the dataloader works
             self.estimated_norm_scaling_factor = 1.0
-            acts = self.next_batch()[0]
+            acts = self.next_batch()[:, 0]
             self.estimated_norm_scaling_factor = None
             norms_per_batch.append(acts.norm(dim=-1).mean().item())
         mean_norm = np.mean(norms_per_batch)

tests/training/test_activations_store.py
@@ -348,8 +348,39 @@ def test_activations_store_estimate_norm_scaling_factor(
     assert isinstance(factor, float)
 
     assert store._storage_buffer is not None
-    scaled_norm = store._storage_buffer[0].norm(dim=-1).mean() * factor
+    scaled_norm = store._storage_buffer[:, 0].norm(dim=-1).mean() * factor
     assert scaled_norm == pytest.approx(np.sqrt(store.d_in), abs=5)
+
+
+@torch.no_grad()
+def test_activations_store_estimate_norm_scaling_factor_uses_full_batch(
+    ts_model: HookedTransformer,
+):
+    """Simulate a model where activation norms within a batch are far
+    apart, and confirm that the norm scaling factor is estimated from
+    the entire batch.
+
+    """
+    tokenizer = ts_model.tokenizer
+    assert tokenizer is not None
+    # Set the embedding of BOS to be very large, and attach the
+    # activations store to a hook that reads it.
+    ts_model.embed.W_E[tokenizer.bos_token_id, :] = 1e4  # type: ignore
+    cfg = build_sae_cfg(prepend_bos=False, context_size=6, hook_name="hook_embed")
+    dataset = Dataset.from_list(
+        [
+            {"text": (tokenizer.bos_token + "a") * 3},  # type: ignore
+        ]
+    )
+    store = ActivationsStore.from_config(ts_model, cfg, override_dataset=dataset)
+    factor = store.estimate_norm_scaling_factor(n_batches_for_norm_estimate=1)
+    # The norm scaling factor should be greater than the factor if it
+    # were only estimated from the 'a' token, and less than if it were
+    # only estimated from bos.
+    mean_from_a = ts_model.embed(ts_model.to_single_token("a")).norm(dim=-1).item()
+    assert factor < np.sqrt(cfg.d_in) / mean_from_a
+    mean_from_bos = ts_model.embed(tokenizer.bos_token_id).norm(dim=-1).item()
+    assert factor > np.sqrt(cfg.d_in) / mean_from_bos
 
 
 def test_activations_store___iterate_tokenized_sequences__yields_concat_and_batched_sequences(