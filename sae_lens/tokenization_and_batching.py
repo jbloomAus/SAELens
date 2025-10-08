090f1321328f4107373acfeb9a37b63201b7c4f0
David Chanin <chanindav@gmail.com>
2025-10-08 13:59:40 +0100

    fixing formatting

--- sae_lens/tokenization_and_batching.py
+++ sae_lens/tokenization_and_batching.py
@@ -89,9 +89,13 @@ def concat_and_batch_sequences(
             ):
                 if len(sequence) >= context_size - 1:
                     begin_sequence_token_id_tensor = torch.tensor(
-                        [begin_sequence_token_id], dtype=torch.long, device=sequence.device
+                        [begin_sequence_token_id],
+                        dtype=torch.long,
+                        device=sequence.device,
                     )
-                    sequence = torch.cat([begin_sequence_token_id_tensor, sequence[:context_size - 1]])
+                    sequence = torch.cat(
+                        [begin_sequence_token_id_tensor, sequence[: context_size - 1]]
+                    )
             if len(sequence) >= context_size:
                 yield sequence[:context_size]
         return
